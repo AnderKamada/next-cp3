1dc7a741276e0a4ad4d36f1473e9ff35e20a70df
Nicolas Martins <nimartins.na1@gmail.com>
2024-03-14 19:24:54 -0300

    Estilização dosComponente Header e Footer

--- src/app/Components/Footer/index.tsx
+++ src/app/Components/Footer/index.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
-    return(
+    return (
         <>
-        <footer>
-            <p>oia o footer aqui</p>
-        </footer>
+            <footer className="bg-gray-800">
+                <p className="text-white text-center">Todos os direitos reservados</p>
+            </footer>
         </>
     );
 };
--- src/app/Components/Header/index.tsx
+++ src/app/Components/Header/index.tsx
@@ -1,15 +1,23 @@
 import Image from "next/image";
 
 const Header = () => {
-    return(
+    return (
         <>
-
-        <header className="bg-black p-12 flex" >
-            <img src="/public/claquete.png" width="120" height="120" alt= "claquete"/>
-                <p className="text-white">oia o header aqui</p>  
-        </header>
+            <header className="bg-black p-12 flex items-center">
+                <Image
+                    className="logo"
+                    src="/claquete.png"
+                    alt="Imagem de uma claquete"
+                    width={100}
+                    height={100}
+                />
+                <h1 className="text-white text-center text-2xl font-bold mx-auto">
+                    aqui só tem filme supimpa
+                </h1>
+            </header>
         </>
     );
 };
 
 export default Header;
+
